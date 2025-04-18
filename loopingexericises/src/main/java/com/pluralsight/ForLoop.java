c52d6a5b51aafc29b9a473186c4cdbc7159f5058
Steven Vargas <stevenvari13@gmail.com>
2025-04-18 15:46:08 -0400

    completed loopingexercise

---
+++ loopingexericises/src/main/java/com/pluralsight/ForLoop.java
@@ -0,0 +1,11 @@
+package com.pluralsight;
+
+public class ForLoop {
+    public static void main(String[] args) {
+
+
+        for (int count = 1; count <= 5; count++){
+            System.out.println("hello world");
+        }
+    }
+}
